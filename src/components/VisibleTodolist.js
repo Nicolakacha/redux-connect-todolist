203fc5ff94c84e48f6e778dd893f8d8481410cea
nicolakacha <nicolakacha@gmail.com>
2020-11-24 20:23:48 +0800

    fix-filter

--- src/components/VisibleTodolist.js
+++ src/components/VisibleTodolist.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import { ALL, COMPLETED, UNCOMPLETED } from '../constants/filterTypes';
 
 class VisibleTodolist extends Component {
   componentDidUpdate = () => {
-    window.localStorage.setItem('todos', JSON.stringify(this.props.todos));
+    window.localStorage.setItem(
+      'todos',
+      JSON.stringify(this.props.todoState.todos)
+    );
   };
 
   render() {
-    return this.props.todos.map((todo) => (
+    const {
+      todoState,
+      filterState,
+      toggleTodoState,
+      deleteTodoState,
+    } = this.props;
+
+    const showedTodos = todoState.todos.filter((todo) => {
+      switch (filterState) {
+        case ALL: {
+          return todo;
+        }
+        case COMPLETED: {
+          return todo.isDone === true;
+        }
+        case UNCOMPLETED: {
+          return todo.isDone === false;
+        }
+        default: {
+          return todo;
+        }
+      }
+    });
+
+    return showedTodos.map((todo) => (
       <TodoItem
         key={todo.id}
         todo={todo}
-        toggleTodoState={this.props.toggleTodoState}
-        deleteTodoState={this.props.deleteTodoState}
+        toggleTodoState={toggleTodoState}
+        deleteTodoState={deleteTodoState}
       />
     ));
   }
--- src/containers/VisibleTodolist.js
+++ src/containers/VisibleTodolist.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import VisibleTodolist from '../components/VisibleTodolist';
 import { deleteTodoState, toggleTodoState } from '../redux/actions';
 
-const mapStateToProps = (state) => state.todoState;
+const mapStateToProps = (state) => state;
 const mapDispatchToProps = { deleteTodoState, toggleTodoState };
 
 export default connect(mapStateToProps, mapDispatchToProps)(VisibleTodolist);
